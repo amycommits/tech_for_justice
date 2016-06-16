3e840da0c0d83edd523c17905d5d77d42c77c97d
Carolyn Hipkins <carolynhipkins.2@gmail.com>
2016-06-16 13:30:16 -0400

    Add files via upload

---
+++ tech4justice-js.js
@@ -0,0 +1,14 @@
+﻿document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='homepage.html'>Home</a></li>" +
+"<li><a href='documents.html'>Documents</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='contact.html'>Contact</a></li>" +
+"</ul>";
+
+$('.nav').on('click', function(el){
+  //remove current active
+  $('.active').removeClass('active')
+  $(this).addClass('active')
+})
+
