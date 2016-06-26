388afb70b118bdc72010ccc983b3219c0208a30d
duaimei <detwiler.amy@gmail.com>
2016-06-26 14:49:42 -0400

    conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626160728) do
+ActiveRecord::Schema.define(version: 20160626175722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "document_user_case_infos", force: :cascade do |t|
     t.integer  "user_document_id"
@@ -72,11 +54,6 @@ ActiveRecord::Schema.define(version: 20160626160728) do
 
   add_index "map_user_document_user_infos", ["user_document_id"], name: "index_map_user_document_user_infos_on_user_document_id", using: :btree
   add_index "map_user_document_user_infos", ["user_information_id"], name: "index_map_user_document_user_infos_on_user_information_id", using: :btree
-
-  create_table "prototypes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "user_arguments", force: :cascade do |t|
     t.integer  "document_id"
