2e9fe9d563d0bd9701d3283f6569f7116e07d69c
Devin Osorio <devinosorio1995@gmail.com>
2016-06-26 11:16:40 -0400

    creats model spec files and certain specs

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -21,6 +21,5 @@
 
 class UserInformation < ActiveRecord::Base
   belongs_to :user
-  has_many :user_documents, through: :document_user_informations
-  has_many :user_case_infos, through: :map_user_document_user_infos
 end
+
---
+++ spec/models/user_document_spec.rb
@@ -0,0 +1,4 @@
+ require "rails_helper"
+
+ RSpec.describe UserDocument, type: :model do
+ end
